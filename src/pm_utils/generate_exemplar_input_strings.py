690e2a9bf63cb5173c0156bef290799e4cd3d5a0
jekwatt <jekwatt@users.noreply.github.com>
2024-06-11 14:14:40 -0500

    Reorder functions

--- src/pm_utils/generate_exemplar_input_strings.py
+++ src/pm_utils/generate_exemplar_input_strings.py
@@ -6,22 +6,6 @@ def main():
     run(args.inp_dump)
 
 
-def normalize_inp(inp_dump):
-    # Remove leading/trailing whitespace characters
-    cleaned_inp_dump = inp_dump.strip()
-    lines = cleaned_inp_dump.splitlines()
-    normalized_inp_str = " ".join(lines)
-    return normalized_inp_str
-
-
-def generate_exemplar_input_str(inp_dump, delimiter=","):
-    normalized_inp_str = normalize_inp(inp_dump)
-    str_list = normalized_inp_str.split()
-    concatenated_str = delimiter.join(str_list)
-    exemplar_input_str = "%" + concatenated_str.replace(delimiter, "%,%") + "%"
-    return exemplar_input_str
-
-
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Generate exemplar input string from inp_dump"
@@ -38,3 +22,19 @@ def parse_args():
 def run(inp_dump):
     exemplar_input_str = generate_exemplar_input_str(inp_dump)
     print(exemplar_input_str)
+
+
+def generate_exemplar_input_str(inp_dump, delimiter=","):
+    normalized_inp_str = normalize_inp(inp_dump)
+    str_list = normalized_inp_str.split()
+    concatenated_str = delimiter.join(str_list)
+    exemplar_input_str = "%" + concatenated_str.replace(delimiter, "%,%") + "%"
+    return exemplar_input_str
+
+
+def normalize_inp(inp_dump):
+    # Remove leading/trailing whitespace characters
+    cleaned_inp_dump = inp_dump.strip()
+    lines = cleaned_inp_dump.splitlines()
+    normalized_inp_str = " ".join(lines)
+    return normalized_inp_str
